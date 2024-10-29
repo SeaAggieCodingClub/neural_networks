15549ea3907fd18731781293f5d6fe838063319e
Panflan <107771496+Panflan@users.noreply.github.com>
2024-10-29 18:22:41 -0500

    someone is adding the menu

--- Snake/__main__.py
+++ Snake/__main__.py
@@ -3,22 +3,3 @@ import time
 import random
 from sys import exit 
 
-
-
-
-width = 800
-height = 650
-
-screen = py.display.set_mode((width, height))
-py.display.set_caption('Snake')
-
-# Set clock speed
-clock = py.time.Clock()
-
-while True:
-    for event in py.event.get():
-        if event.type == py.QUIT:
-            py.quit()
-            exit()
-    py.display.update
-    clock.tick(60)
--- Snake/movement_snake.py
+++ Snake/movement_snake.py
@@ -2,13 +2,6 @@ import pygame
 
 pygame.init()
 
-# Create a display surface
-width = 800
-height = 400
-
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Snake Game')
-# delete above later
 
 snake_speed = 20
 
