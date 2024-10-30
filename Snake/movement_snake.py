e46b74cf11f927f7e0504359445d33c5554b91ed
Panflan <107771496+Panflan@users.noreply.github.com>
2024-10-30 11:59:21 -0500

    building off snake movement

--- Snake/movement_snake.py
+++ Snake/movement_snake.py
@@ -2,19 +2,33 @@ import pygame
 
 pygame.init()
 
+cell_number = 40
+cell_size = 20
+
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 
 snake_speed = 20
 snake_head = pygame.image.load('Snake/snake head.png').convert()
-green = (0, 255, 0)
+white = pygame.color.Color('#FFFFFF')
+
 snake_x = 20
 snake_y = 20
 finished = False
+
 while not finished:
-    keys = pygame.key.get_pressed
-    if keys[pygame.K_up]:
+    screen.fill(white)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
         snake_y -= 1
-
-
-#def movement_snake(snake_block, snake_list):
-#    for x in snake_list:
-#        pygame.draw.rect(screen, green, [x[0], x[1], snake_block, snake_block])
+    if keys[pygame.K_DOWN]:
+            snake_y += 1
+    if keys[pygame.K_LEFT]:
+            snake_x -= 1
+    if keys[pygame.K_RIGHT]:
+         snake_x += 1
+    screen.blit(snake_head, (snake_x,snake_y))
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+            finished = True
+pygame.quit()
