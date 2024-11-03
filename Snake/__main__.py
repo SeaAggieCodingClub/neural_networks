9f36a5b674959d11cfccee0895447dcfa47fdb4e
RylanFerguson <rylankferguson@gmail.com>
2024-11-02 22:48:49 -0500

    made the menu functional

--- Snake/__main__.py
+++ Snake/__main__.py
@@ -2,7 +2,7 @@ import pygame
 import time
 import random
 from sys import exit
-
+from button import Button
 
 pygame.init()
 
@@ -13,75 +13,69 @@ cell_size = 20
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 pygame.display.set_caption("Snake")
 clock = pygame.time.Clock()
-main_font = pygame.font.SysFont("Arial", 30)
-
-class Button():
-    def __init__(self, image, x_pos, y_pos, text_input):
-        self.image = image
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
-        self.text_input = text_input
-        self.text = main_font.render(self.text_input, True, "white")
-        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
-
-    def update(self):
-        screen.blit(self.image, self.rect)
-        screen.blit(self.text, self.text_rect)
-
-    def check_for_input(self, position):
-        if (position[0] in range(self.rect.left, self.rect.right)) and (position[1] in range(self.rect.top, self.rect.bottom)):
-            print("Button pressed")
-
-    def change_color(self, position):
-        if (position[0] in range(self.rect.left, self.rect.right)) and (position[1] in range(self.rect.top, self.rect.bottom)):
-            self.text = main_font.render(self.text_input, True, "green")
-        else:
-            self.text = main_font.render(self.text_input, True, "white")
+main_font = pygame.font.SysFont("Arial", 26)
 
 # Button variables
 button_surface = pygame.image.load("Snake/basic_button.png").convert()
-button_surface = pygame.transform.scale(button_surface, (300, 65))
-
-play_button = Button(button_surface, 400, 300, "Play")
+button_surface = pygame.transform.scale(button_surface, (235, 55))
 
 # Main menu screen
 def main_menu():
     while True:
+        menu_mouse_pos = pygame.mouse.get_pos()
+
+        screen.fill("white")
+
+        play_button = Button(button_surface, 400, 300, "Play", main_font, "white", "green")
+        options_button = Button(button_surface, 400, 370, "Options", main_font, "white", "green")
+        exit_button = Button(button_surface, 400, 440, "Exit", main_font, "white", "green")
+
+        for button in [play_button, options_button, exit_button]:
+            button.change_color(menu_mouse_pos)
+            button.update(screen)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                play_button.check_for_input(pygame.mouse.get_pos())
+                if play_button.check_for_input(menu_mouse_pos):
+                    play()
+                if options_button.check_for_input(menu_mouse_pos):
+                    options()
+                if exit_button.check_for_input(menu_mouse_pos):
+                    pygame.quit()
+                    exit()
 
         pygame.display.update()
-        screen.fill("white")
-
-        play_button.update()
-        play_button.change_color(pygame.mouse.get_pos())
-
         clock.tick(60)
 
+# Options screen
+def options():
+    print("Options screen")
 
 
 # Play screen
 def play():
     while True:
-        # Checks if the window is closed then terminates the program
         for event in pygame.event.get():
+            # Checks if the window is closed then terminates the program
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+            # Checks if escape key is pressed then opens the menu screen
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    main_menu()
 
         # Draw all elements
         pygame.display.update()
         screen.fill((172, 206, 96))
         
-
         # Frame rate
         clock.tick(60)
-play()
+
+main_menu()
 
 
 
